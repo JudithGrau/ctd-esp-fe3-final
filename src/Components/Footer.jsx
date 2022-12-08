44a87f8c313a1727c867ebd28d8334d333a72987
JudithGrau <judith.betina.grau@gmail.com>
2022-12-08 20:43:12 -0300

    Modifico el Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import IconButton from '@mui/material/IconButton';
-import { Toolbar, Tooltip } from '@mui/material';
+import { Tooltip } from '@mui/material';
 import { socialNetwork } from './socialNetwork';
 import { Box } from '@mui/material';
-import { grey } from '@mui/material/colors';
 
 const Footer = () => {
   return (
+  <div style={{backgroundColor:"#d1cfcf"}}>
+    
     <footer>
-      <Box><img src="./images/DH.png" alt='DH-logo' /></Box>
+      <Box><img src="../images/DH.png" alt='DH-logo' /></Box>
       <Box>
-      {
-            socialNetwork.map(({ linkto, snTitle, Icon }) => (
-              
-                <Tooltip key={snTitle} title={snTitle}>
-                  <IconButton >
-                    <a  style={{color:"#000"}} href={linkto} target="e_blank" rel="nonreferrer"><Icon /></a>
-                  </IconButton>
-                </Tooltip>
-              
-            ))
-          }
-        </Box>
+        {
+          socialNetwork.map(({ linkto, snTitle, Icon}) => (
+              <Tooltip key={snTitle} title={snTitle}>
+                <IconButton>
+                  <a href={linkto} target="e_blank" rel="nonreferrer"><Icon /></a>
+                </IconButton>
+              </Tooltip>
+          ))
+        }
+      </Box>
     </footer>
+  </div>
   )
 }
 
