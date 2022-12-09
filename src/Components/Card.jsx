d7c27f585d2d588da0b7d94523ed1e9452d3700a
micaelabentos <bentosmicaela@gmail.com>
2022-12-09 01:58:54 -0300

    Arreglando problema con alert y agregando algunos estilos

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,4 @@
-import {React, useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import BasicCard from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -12,42 +12,39 @@ const Card = (props) => {
 
   const [like, setLike] = useState(false)
   const [favsSelected, setFavsSelected] = useState([])
-  
-useEffect(() => {
-  const favorites = localStorage.getItem("favorites");
-  if (favorites){
-    const favoritesParsed = JSON.parse(favorites)
-    setFavsSelected(favoritesParsed)
-  }
-}, [like, props.id])
 
-  
+  useEffect(() => {
+    const favorites = localStorage.getItem("favorites");
+    if (favorites) {
+      const favoritesParsed = JSON.parse(favorites)
+      setFavsSelected(favoritesParsed)
+    }
+  }, [like, props.id])
+
+  const handleLikedStatus = () => {
+    if (favsSelected) {
+      const selectedFav = favsSelected.some(fav => fav.id === props.id)
+      return selectedFav ? "★" : "✰"
+    }
+  }
 
   const addFav = () => {
     setLike(!like)
     const favorites = localStorage.getItem("favorites");
-    if(favorites){
-    let favParsed = JSON.parse(favorites);
-    const filteredDentist = favParsed.filter(item => item.id !== props.id);
-    const exist = filteredDentist.length !== favParsed.length;
-    exist ? favParsed = filteredDentist : favParsed.push({...props, liked:{isLiked: true, id: props.id}});
-    localStorage.setItem("favorites", JSON.stringify(favParsed));
-    alert("Dentista agregado exitosamente!");
-  } else {
-    localStorage.setItem("favorites", JSON.stringify([{...props, liked:{isLiked: true, id: props.id}}]))
-  }
-  }
+    if (favorites) {
+      let favParsed = JSON.parse(favorites);
+      const filteredDentist = favParsed.filter(item => item.id !== props.id);
+      const exist = filteredDentist.length !== favParsed.length;
+      exist ? favParsed = filteredDentist : favParsed.push({ ...props, liked: { isLiked: true, id: props.id } });
+      localStorage.setItem("favorites", JSON.stringify(favParsed))
+      handleLikedStatus() === "✰" && alert("Dentista agregado exitosamente!");
+    } else {
+      localStorage.setItem("favorites", JSON.stringify([{ ...props, liked: { isLiked: true, id: props.id } }]))
 
-  const handleLikedStatus = () => {
-    if(favsSelected){
-      const  selectedFav = favsSelected.some(fav => fav.id === props.id)
-        return selectedFav ? "★" : "✰"
     }
   }
 
-  /* En cada card deberan mostrar en name - username y el id */ 
-  /* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */ 
-  /* Ademas deberan integrar la logica para guardar cada Card en el localStorage */ 
+  
   return (
     <BasicCard className="card" >
       <CardActionArea>
@@ -63,14 +60,14 @@ useEffect(() => {
               {props.name}
             </Typography>
             <Typography data-testid="cardName" sx={{ textAlign: "center" }} variant="subtitle2" color="text.secondary">
-            {props.username}
-          </Typography>
+              {props.username}
+            </Typography>
           </Link>
         </CardContent>
       </CardActionArea>
 
       <Button data-testid="likeButton" onClick={addFav} className="favButton">
-          {handleLikedStatus()} 
+        {handleLikedStatus()}
       </Button>
 
     </BasicCard>
